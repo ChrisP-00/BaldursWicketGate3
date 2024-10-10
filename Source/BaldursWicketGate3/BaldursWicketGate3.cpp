5f5a76e378036d74f16e70f0990aeeab65585275
ChrisP <jspark4151@gmail.com>
2024-10-10 19:46:57 +0900

    Initial commit

---
+++ Source/BaldursWicketGate3/BaldursWicketGate3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BaldursWicketGate3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BaldursWicketGate3, "BaldursWicketGate3" );
