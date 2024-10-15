6129596bb80fd254a83058cf43947afa9abf4c7c
ChrisP <jspark4151@gmail.com>
2024-10-15 16:46:53 +0900

    Feat: Add Default Game Mode

---
+++ BaldursWicketGate3/Source/BaldursWicketGate3/BaldursWicketGate3GameMode.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaldursWicketGate3GameMode.h"
+
+#include "MainCharacterController.h"
+
+ABaldursWicketGate3GameMode::ABaldursWicketGate3GameMode()
+{
+	/* Set Player Controller Class */
+	PlayerControllerClass = AMainCharacterController::StaticClass();
+
+	/* Set defaultc controller  */
+	
+}
