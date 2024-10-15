88e2bc633f27bd7d1963a8219097f054978bede0
ChrisP <jspark4151@gmail.com>
2024-10-15 16:47:19 +0900

    Feat: Add Main Character

---
+++ BaldursWicketGate3/Source/BaldursWicketGate3/MainCharacter.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+
+#include "AIController.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	/*// Don't rotate character to camera direction
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;*/
+	
+	/*/* Set Camera Arm Spring #1#
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	RootComponent = SpringArm;
+	SpringArm->TargetArmLength = CameraArmLength;
+	SpringArm->SetRelativeRotation(CameraArmAngle);
+
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+
+	/* Create Camera #1#
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Comp"));
+	CameraComp->SetupAttachment(SpringArm);
+	CameraComp->bUsePawnControlRotation = false;*/
+}
+
+void AMainCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	FRotator InitialRotation = FRotator(0, 0, 0);
+	SetActorRotation(InitialRotation);
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ BaldursWicketGate3/Source/BaldursWicketGate3/MainCharacter.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class BALDURSWICKETGATE3_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+	virtual void PossessedBy(AController* NewController) override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	/*UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCapsuleComponent* CapsuleComp;*/
+	/*
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComp;
+	*/
+
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float CameraArmLength = 800.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	FRotator CameraArmAngle = FRotator(-60.f, 0.f, 0.f);*/
+};
